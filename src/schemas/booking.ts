9557499611ebedd6b0033ed09972150b44eda57d
Paris Smirlakis <smirlakis@gmail.com>
2024-08-27 17:43:52 +0300

    add error handling for unrecognized_keys create booking api

--- src/schemas/booking.ts
+++ src/schemas/booking.ts
@@ -6,6 +6,19 @@ const isISODate = (dateString: string): boolean => {
   return isoDatePattern.test(dateString);
 };
 
+export const bookingCreateUserInputSchema = z.object({
+  hotel: z.string().min(1).max(100),
+  location: z.string().min(1).max(100),
+  numberOfGuests: z.number().min(1).max(10),
+  numberOfRooms: z.number().min(1).max(10),
+  checkIn: z.string().refine(isISODate, {
+    message: "Invalid date format. Must be ISO 8601 date string.",
+  }),
+  checkOut: z.string().refine(isISODate, {
+    message: "Invalid date format. Must be ISO 8601 date string.",
+  }),
+}).strict();  // strict mode to disallow extra fields
+
 export const bookingCreateSchema = z.object({
   hotel: z.string().min(1).max(100),
   location: z.string().min(1).max(100),
--- src/server/services/BookingService.ts
+++ src/server/services/BookingService.ts
@@ -1,4 +1,4 @@
-import { bookingCreateSchema } from "@/schemas";
+import { bookingCreateUserInputSchema } from "@/schemas";
 import type { BookingDto } from "@/shared";
 import { Booking } from "@prisma/client";
 import { Inject, Service } from "typedi";
@@ -27,7 +27,7 @@ export class BookingService {
     return mappedData;
   }
 
-  @ValidateInput(bookingCreateSchema)
+  @ValidateInput(bookingCreateUserInputSchema)
   async create(request: BookingDto): Promise<string> {
     console.log(request);
     const verificationData = await this.verifier.initVerification();
@@ -38,16 +38,7 @@ export class BookingService {
     const data = this.mapper
       .get()
       .map<BookingDto, Booking>(request, "BookingDto", "Booking"); 
-      /* TODO : change DTO to 
-      {
-          "hotel": "Grand hotel",
-          "location": "The city",
-          "numberOfGuests": 3,
-          "numberOfRooms": 3,
-          "checkIn": "2011-10-05T14:48:00.000Z",
-          "checkOut": "2011-12-05T14:48:00.000Z"
-      }
-      */
+      
     const dbRecord = await this.bookingRepository.create(data);
     console.log(dbRecord);
     //TODO error handling {statusCode, message}
