11ba1a6ea106b2ac2c0a8fd472fe36dac5170a5b
Rania Tsolka <Ourania.TSOLKA@netcompany.com>
2024-09-11 17:07:48 +0300

    retrieve booking details - server side on confirmation page

--- src/pages/confirmation/[id].tsx
+++ src/pages/confirmation/[id].tsx
@@ -1,6 +1,30 @@
-import BookingConfirmation from "@/client/components/BookingConfirmation";
+import BookingConfirmation from "@/client/components/confirmation";
+import { BookingService } from "@/server";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import { AppProps } from "next/app";
+import Container from "typedi";
 
 export default function ConfirmationPage(props: AppProps) {
-  return <BookingConfirmation />;
+  const { id, ...rest } = props.pageProps;
+  if (Object.keys(rest).length === 0) {
+    return (
+      <div className='text-black text-lg font-bold p-4 '>Booking not found</div>
+    );
+  }
+  return <BookingConfirmation details={rest} id={id} />;
 }
+
+// Use dynamic id from the URL
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const { id } = context.params as { id: string };
+  const bookingService = Container.get(BookingService);
+  const response = await bookingService.bookingDetails(id);
+  return {
+    props: {
+      id,
+      ...response,
+    },
+  };
+};
