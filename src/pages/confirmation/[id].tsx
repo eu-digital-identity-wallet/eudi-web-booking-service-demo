7e9edbac60092f7cb1d4d56bee975659c7eb0782
Rania Tsolka <ourania.tsolka@netcompany.com>
2024-09-12 11:38:55 +0300

    handle bookingId confirmation - not found

--- src/pages/confirmation/[id].tsx
+++ src/pages/confirmation/[id].tsx
@@ -6,13 +6,19 @@ import { AppProps } from "next/app";
 import Container from "typedi";
 
 export default function ConfirmationPage(props: AppProps) {
-  const { id, deviceType, ...rest } = props.pageProps;
-  if (Object.keys(rest).length === 0) {
+  const { id, deviceType, hasError, ...bookingDetails } = props.pageProps;
+  if (hasError) {
     return (
       <div className='text-black text-lg font-bold p-4 '>Booking not found</div>
     );
   }
-  return <BookingConfirmation details={rest} id={id} deviceType={deviceType} />;
+  return (
+    <BookingConfirmation
+      details={bookingDetails}
+      id={id}
+      deviceType={deviceType}
+    />
+  );
 }
 
 // Use dynamic id from the URL
@@ -23,13 +29,16 @@ export const getServerSideProps: GetServerSideProps = async (
 
   const { id } = context.params as { id: string };
   const bookingService = Container.get(BookingService);
-  const response = await bookingService.bookingDetails(id);
-
-  return {
-    props: {
-      id,
-      deviceType,
-      ...response,
-    },
-  };
+  let properties = {};
+  try {
+    const bookingDetails = await bookingService.bookingDetails(id);
+    if (bookingDetails) {
+      properties = { id, deviceType, ...bookingDetails, hasError: false };
+    } else {
+      properties = { hasError: true };
+    }
+  } catch (error) {
+    properties = { hasError: true };
+  }
+  return { props: properties };
 };
