561467bc45d50fd700a7558152b9c9c162471b0e
Paris Smirlakis <smirlakis@gmail.com>
2024-09-23 17:15:40 +0300

    datepicker fix

--- src/client/components/atoms/DatePickerField.tsx
+++ src/client/components/atoms/DatePickerField.tsx
@@ -1,6 +1,7 @@
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { TextField } from '@mui/material';
 import React from 'react';
+import dayjs from 'dayjs';
 
 interface DatePickerFieldProps {
   label: string;
@@ -13,8 +14,14 @@ export const DatePickerField: React.FC<DatePickerFieldProps> = ({ label, value, 
   return (
     <DatePicker
       label={label}
-      value={value}
-      onChange={onChange}
+      value={value ? dayjs(value) : null} 
+      onChange={(newValue) => {
+        if (newValue) {
+          onChange(newValue.toDate()); // Convert Dayjs back to Date
+        } else {
+          onChange(null);
+        }
+      }}
       sx={{minWidth:'240px',width:'100%'}}
       slotProps={{
         textField: {
