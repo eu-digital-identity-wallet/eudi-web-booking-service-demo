e6df642ba4d1e0f8583d292509fc9a66f2297f4c
vafeini <129304399+vafeini@users.noreply.github.com>
2024-09-25 16:31:18 +0300

    Presentation definition requesting mDL and PhotoId attestations (#23)

--- src/server/types/payload.ts
+++ src/server/types/payload.ts
@@ -2,23 +2,27 @@ export type Payload = {
   type: string;
   presentation_definition: {
     id: string;
-    input_descriptors: {
-      id: string;
-      name: string;
-      purpose: string;
-      format: {
-        mso_mdoc: { alg: string[] };
-      };
-      constraints: {
-        fields: {
-          path: string[];
-          intent_to_retain: boolean;
-        }[];
-      };
-    }[];
+    input_descriptors: InputDescriptor[];
   };
   jar_mode: string;
   presentation_definition_mode: string;
   nonce: string;
   wallet_response_redirect_uri_template?: string; // Optional property
 };
+
+export type InputDescriptor = {
+  id: string;
+  name: string;
+  purpose: string;
+  format: {
+    mso_mdoc: { alg: string[] };
+  };
+  constraints: {
+    fields: FieldConstraint[];
+  };
+}
+
+export type FieldConstraint = {
+  path: string[];
+  intent_to_retain: boolean;
+}
